f7ab96c7ef99a8420a1a11a40a7cf5d96ac01f11
Oleksii Haiduk <oleksiihaiduk@curiosity-2.local>
2024-05-18 14:23:52 +0300

    add homework 4

---
+++ jsBasics/task3.js
@@ -0,0 +1,12 @@
+const personAge = 33;
+const adultAge = 18;
+
+if (personAge >= adultAge) {
+    console.log(`person is adult`)
+    console.log(personAge);
+    console.log(adultAge);
+} else {
+    console.log(`person is not adult`);
+    console.log(personAge);
+    console.log(adultAge);
+} 
---
+++ jsBasics/tasks4.3.js
@@ -0,0 +1,6 @@
+const RADIUS = 28;
+const HEIGH = 47;
+
+const res = (Math.PI * (RADIUS^ 2 * HEIGH)).toFixed(2);
+console.log(res);
+
