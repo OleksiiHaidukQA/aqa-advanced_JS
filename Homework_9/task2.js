346ad986036f0a9726e487f0512a9fdc3ec9261e
Oleksii Haiduk <oleksiihaiduk@curiosity-2.local>
2024-06-10 18:11:22 +0300

    Homework_9

---
+++ Homework_9/task2.js
@@ -0,0 +1,10 @@
+const book = {
+    title: "JavaScript and jQuery: Interactive Front-End Web Development",
+    author: "Jon Duckett",
+    year: "2014",
+};
+
+const {title, author,} = book;
+
+console.log(title);
+console.log(author);
